40e999f2dc922a2ae055996dced37da22a9f41ac
Christian Shelton <cshelton@cs.stanford.edu>
2020-07-10 13:54:53 -0700

    Fixed Counter.sortedKeys for Python 3

--- util.py
+++ util.py
@@ -293,8 +293,7 @@ class Counter(dict):
         ['second', 'third', 'first']
         """
         sortedItems = list(self.items())
-        compare = lambda x, y:  sign(y[1] - x[1])
-        sortedItems.sort(cmp=compare)
+        sortedItems.sort(key=(lambda x: x[1]))
         return [x[0] for x in sortedItems]
 
     def totalCount(self):
